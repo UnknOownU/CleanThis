699dfae1e273a710bf893ccaec88594012c5d21d
UnknOownU <149084644+UnknOownU@users.noreply.github.com>
2024-03-19 15:09:14 +0100

    avancement

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -83,24 +83,7 @@ class DashboardController extends AbstractDashboardController
             
             yield MenuItem::section('Principal');
             yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-            yield MenuItem::subMenu('Opérations', 'fa fa-broom')->setSubItems([
-                MenuItem::linkToCrud('Toutes les opérations', 'fa fa-tags', Operation::class),
-                MenuItem::linkToCrud('En attente de Validation', 'fa fa-clock-o', Operation::class)
-                ->setQueryParameter('status', 'En attente de Validation'),
-                MenuItem::linkToCrud('En cours', 'fa fa-arrow-right', Operation::class)
-                    ->setQueryParameter('status', 'En cours'),
-                MenuItem::linkToCrud('Terminées', 'fa fa-check', Operation::class)
-                    ->setQueryParameter('status', 'Terminée'),
-            ]);
 
-            yield MenuItem::section('Support');
-            yield MenuItem::linkToRoute('Paramètres', 'fa fa-cogs', 'settings_route');
-            yield MenuItem::linkToRoute('Besoin D’aide ?', 'fa fa-question-circle', 'help_route');
-            yield MenuItem::linkToRoute('Chat', 'fa fa-comments', 'chat_route');
-        }
-        if ($this->isGranted('ROLE_APPRENTI')) {
-            yield MenuItem::section('Principal');
-            yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
             yield MenuItem::subMenu('Opérations', 'fa fa-broom')->setSubItems([
                 MenuItem::linkToCrud('Toutes les opérations', 'fa fa-tags', Operation::class),
                 MenuItem::linkToCrud('En attente de Validation', 'fa fa-clock-o', Operation::class)
@@ -114,10 +97,30 @@ class DashboardController extends AbstractDashboardController
             yield MenuItem::linkToRoute('Historique', 'fa fa-history', 'history_route');
 
             yield MenuItem::section('Support');
-            yield MenuItem::linkToRoute('Paramètres', 'fa fa-cogs', 'settings_route');
-            yield MenuItem::linkToRoute('Besoin D’aide ?', 'fa fa-question-circle', 'help_route');
-            yield MenuItem::linkToRoute('Chat', 'fa fa-comments', 'chat_route');
+            yield MenuItem::linkToCrud('Votre Profil', 'fa fa-user', User::class);
         }
+    
+        if ($this->isGranted('ROLE_APPRENTI')) {
+            
+            yield MenuItem::section('Principal');
+            yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+
+            yield MenuItem::subMenu('Opérations', 'fa fa-broom')->setSubItems([
+                MenuItem::linkToCrud('Toutes les opérations', 'fa fa-tags', Operation::class),
+                MenuItem::linkToCrud('En attente de Validation', 'fa fa-clock-o', Operation::class)
+                ->setQueryParameter('status', 'En attente de Validation'),
+                MenuItem::linkToCrud('En cours', 'fa fa-arrow-right', Operation::class)
+                    ->setQueryParameter('status', 'En cours'),
+                MenuItem::linkToCrud('Terminées', 'fa fa-check', Operation::class)
+                    ->setQueryParameter('status', 'Terminée'),
+            ]);
+
+            yield MenuItem::linkToRoute('Historique', 'fa fa-history', 'history_route');
+
+            yield MenuItem::section('Support');
+            yield MenuItem::linkToCrud('Votre Profil', 'fa fa-user', User::class);
+        }
+    
         if ($this->isGranted('ROLE_CUSTOMER')) {
             
             yield MenuItem::section('Principal');
