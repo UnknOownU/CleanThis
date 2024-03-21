c15bf0ba4868bea8e382fe170ed308abd69ec712
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-03-21 13:35:15 +0100

    Traduction fr/en terminée, ajout drapeaux de choix de langue dans la navbar

---
+++ src/Controller/MainController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/change-locale/{locale}', name: 'change_locale')]
+    public function changeLocale($locale, Request $request): Response
+    {
+        //Stocker la langue demandée dans la session
+        $request->getSession()->set('_locale', $locale);
+        //Revenir sur la page
+        return $this->redirect($request->headers->get('referer'));
+    }
+}
