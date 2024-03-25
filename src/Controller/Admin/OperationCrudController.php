cc92df98b7fa45f5000505b58082476f4f9814f3
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-03-25 15:54:37 +0100

    Réimplantation de la pagination dans les configureCrud des dashboard membres et operations

--- src/Controller/Admin/OperationCrudController.php
+++ src/Controller/Admin/OperationCrudController.php
@@ -50,7 +50,9 @@ class OperationCrudController extends AbstractCrudController {
         return $crud
             ->overrideTemplate('crud/new', 'operation_crud/new.html.twig')
             ->overrideTemplate('crud/edit', 'operation_crud/edit.html.twig')
-            ->setSearchFields(null);
+            ->setSearchFields(['name', 'type', 'status'])
+            ->setPaginatorPageSize(7)            
+            ->setPaginatorRangeSize(0);
             $statusFilter = $this->getContext()->getRequest()->query->get('status');
             if ($statusFilter) {
                 $crud->setDefaultSort(['status' => $statusFilter]);
