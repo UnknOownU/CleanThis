1a8e5cddeb1f9a4cdd2629907d2b2fab418a0a59
UnknOownU <149084644+UnknOownU@users.noreply.github.com>
2024-04-24 13:35:09 +0200

    Ajout

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,13 +4,16 @@ namespace App\Controller;
 
 use Exception;
 use App\Entity\User;
+use App\Entity\Operation;
 use App\Form\UserType;
 use App\Service\LogsService;
 use App\Repository\UserRepository;
+use App\Repository\OperationRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/')]
@@ -23,5 +26,31 @@ class UserController extends AbstractController
             'users' => $userRepository->findAll(),
         ]);
     }
+
+
+    #[Route('/users', name: 'api_users_by_roles', methods: ['GET'])]
+    public function getUsersByRoles(UserRepository $userRepository, OperationRepository $operationRepository): JsonResponse
+    {
+        $roles = ['ROLE_ADMIN', 'ROLE_SENIOR', 'ROLE_APPRENTI'];
+        $users = $userRepository->findByRoles($roles);
+    
+        $formattedUsers = array_map(function ($user) use ($operationRepository) {
+            $assignedOperations = $operationRepository->findAssignedOperationsByUser($user->getId());
+            $assignedOperationsNames = array_map(function ($operation) {
+                return $operation['name']; // Assurez-vous que 'name' est correctement sélectionné dans la requête
+            }, $assignedOperations);
+    
+            return [
+                'id' => $user->getId(),
+                'email' => $user->getEmail(),
+                'roles' => $user->getRoles(),
+                'name' => $user->getName(),
+                'firstname' => $user->getFirstname(),
+                'assignedOperationsNames' => $assignedOperationsNames,
+            ];
+        }, $users);
+        return $this->json($formattedUsers);
+    }
+    
 
 }
