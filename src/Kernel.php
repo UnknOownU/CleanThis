d3cd2e666661c0c2dba05b66f433821be98f8f05
UnknOownU <149084644+UnknOownU@users.noreply.github.com>
2024-04-29 09:22:37 +0200

    ajout d'un cache en plus et reduction des lags

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
@@ -8,4 +7,20 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function getCacheDir(): string
+    {
+        return $this->computeCacheDir();
+    }
+
+    private function computeCacheDir(): string
+    {
+        // For Docker performance
+        if ($this->getEnvironment() === 'test' || $this->getEnvironment() === 'dev') {
+            return '/tmp/'.$this->environment;
+        } else {
+            return $this->getProjectDir().'/var/cache/'.$this->environment;
+        }
+    }
 }
+?>
